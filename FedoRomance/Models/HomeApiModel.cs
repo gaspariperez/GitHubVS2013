4f6e0841f9cedf9d5998a7e686529ff356b12079
gaspariperez <gaspariperez@gmail.com>
2015-01-06 10:43:55 +0100

    ApiController + ApiModel

---
+++ FedoRomance/Models/HomeApiModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FedoRomance.Web.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        
+
+
+    }
+
+
+
+}
