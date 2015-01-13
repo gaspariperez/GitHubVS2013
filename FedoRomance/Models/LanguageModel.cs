4ab257e12b9a03d6217046d1d3e03ee3e240f3b0
HenrikHo <Henrik.yuan.ho@gmail.com>
2015-01-13 22:35:36 +0100

    Henrik-san is sorry

---
+++ FedoRomance/Models/LanguageModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using sprak;
+
+namespace FedoRomance.Web.Models
+{
+    public class LanguageModel
+    {
+        [Display(Name = "Profile", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceType = typeof(Language))]
+        public string Profile { get; set; }
+
+        [Display(Name = "Edit", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceType = typeof(Language))]
+        public string Edit { get; set; }
+
+        [Display(Name = "Friends", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceType = typeof(Language))]
+        public string Friends { get; set; }
+
+        [Display(Name = "Search", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceType = typeof(Language))]
+        public string Search { get; set; }
+
+        [Display(Name = "LogOut", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceType = typeof(Language))]
+        public string LogOut { get; set; }
+
+        [Display(Name = "LogIn", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceType = typeof(Language))]
+        public string LogIn { get; set; }
+
+    }
+}
