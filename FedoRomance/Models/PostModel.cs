5da9224a6ee7e7f6cf46ee5d41c28ea8ec619d4d
gaspariperez <gaspariperez@gmail.com>
2015-01-13 15:13:57 +0100

    First commit

---
+++ FedoRomance/Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FedoRomance.Web.Models
+{
+    public class PostModel
+    {
+        public int PostId { get; set; }
+        public string Message { get; set; }
+        public string PostedBy { get; set; }
+        public string PostedDate { get; set; }
+	}
+}
